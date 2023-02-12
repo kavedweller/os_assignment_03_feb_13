c7e9b83ce806f067affdbeb9aeaf643a809c9c1b
A Kabir Chowdhury <anisulkabir.c@gmail.com>
2023-02-13 01:43:33 +0600

    added: problem-2 solution.

--- php_assignment_3.php
+++ php_assignment_3.php
@@ -17,4 +17,15 @@ function intParityCheck($num)
 echo "Number 11 is " . intParityCheck(11) . "\n";
 echo "Number 8 is " . intParityCheck(8) . "\n";
 
+
+
+echo "\r\n";
+/*
+ * 1+2+3...…….100  Write a loop to calculate the summation of the series.
+ */
+$sum = 0;
+for ($i = 1; $i <= 100; $i++) {
+    $sum += $i;
+}
+echo "Sum of the series is " . $sum;
 ?>
